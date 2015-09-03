d02f6e461d6f41f897dd4bd0a112e8a43a712d6e
Tyler Funk <tylerfunk@Alveyworld-25.local>
2015-09-03 09:12:45 -0600

    added Band Name Generator

---
+++ Basic.py
@@ -0,0 +1,13 @@
+#write a program that takes a person's name
+# and outputs a random name and outputs a name for a band
+import sys
+
+#randomly choose a title for the persons name
+#randomly choose an adjective to describe the band
+#randomly choose a plural noun to name the band
+
+def main(args):
+	print "this is fun"
+	
+if __name__ == "__main__":
+	main(sys.argv)	
